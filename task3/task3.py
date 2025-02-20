91dbdb20fc595b2e42c607c0725749e0f0fd8418
Boroda-v-goroshek <n.shestakov@g.nsu.ru>
2025-02-21 02:16:42 +0700

    add task3

--- task3/task3.py
+++ task3/task3.py
@@ -8,32 +8,42 @@ from selenium.webdriver.edge.options import Options
 from bs4 import BeautifulSoup
 
 
-#object = "Носки"
-
+# Настройка
 options = Options()
 options.add_argument('--ignore-certificate-errors')
+options.add_argument("--start-maximized")
 driver = webdriver.Edge(options=options)
-driver.get('https://daitres.com')
+driver.get('https://hempforlife.ru/login/')
 
-# Авторизациz
+
+# Авторизация
+driver.find_element(By.XPATH, "//input[@title='Логин']").send_keys("n.shestakov@g.nsu.ru")
+driver.find_element(By.XPATH, "//input[@type='password']").send_keys("RAlf2005")
+driver.find_element(By.XPATH, "//input[@type='submit']").click()
 time.sleep(1)
-driver.find_element(By.XPATH, "//a[@href='https://daitres.com/my-account']").click()
+
+
+# Поиск страницы
+driver.find_element(By.XPATH, "//div[@class='f-cookies__button']").click()
+driver.find_element(By.XPATH, "//div[@data-id='83']").click()
+driver.find_element(By.XPATH, "//div[@class='b-catalog__button']").click()
 time.sleep(1)
-driver.find_element(By.XPATH, "//input[@id='nasa_username']").send_keys("n.shestakov@g.nsu.ru")
-driver.find_element(By.XPATH, "//input[@id='nasa_password']").send_keys("RAlf2005")
-driver.find_element(By.XPATH, "//button[@type='submit']").click()
-time.sleep(3)
-
-# Переход к нужной странице
-katalog = driver.find_element(By.XPATH, "//a[@href='https://daitres.com/muzhskie-trusy-skidki']").click()
-time.sleep(3)
-# Получение данных
+driver.find_element(By.XPATH, "//a[@href='/category/postelnye-prinadlezhnosti/']").click()
+time.sleep(1)
 
 
+# Сбор данных
+driver.find_element(By.XPATH, "//button[@class='h-yet__show']").click()
+title = driver.find_element(By.PARTIAL_LINK_TEXT, "Одеяла и подушки").text
+data_list = driver.find_elements(By.XPATH, "//div[@class='products__item products__item-flying']")
+
+# Запись в таблицу
+with open("output.csv", 'w', encoding='utf-8') as w_file:
+    file_writer = csv.writer(w_file, delimiter = ',')
 
 
+    for data in data_list:
+        text = data.text.split('\n')
+        file_writer.writerow(text)
 
-
-
-input()
 driver.quit()
